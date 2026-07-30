93127a650167507992a9f22b91eb80fc47c55be9
kongzhilv <157718507+kongzhilv@users.noreply.github.com>
2026-07-30 21:15:51 +0800

    修复：移除 Compose weight 内部属性导入

--- core/ui/src/main/java/one/only/player/core/ui/components/NextDialog.kt
+++ core/ui/src/main/java/one/only/player/core/ui/components/NextDialog.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
