68fbf0d7883f9a8d3ce7e8862e26bdadc44106a7
kongzhilv <157718507+kongzhilv@users.noreply.github.com>
2026-07-31 12:33:37 +0800

    发行：版本更新至 1.0.148

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         applicationId = "io.github.kongzhilv.onlyplayer"
-        versionCode = 148
-        versionName = "1.0.147"
+        versionCode = 149
+        versionName = "1.0.148"
     }
 
     buildFeatures {
